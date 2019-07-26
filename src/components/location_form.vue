135a142ccb5626b2c708ee08f4abd5c90602c825
Steven Cheng <steven.cheng@teksbotics.com>
2019-07-27 01:37:41 +0800

    loc form

--- src/components/location_form.vue
+++ src/components/location_form.vue
@@ -2,18 +2,28 @@
 div 
   h2.title Location Information
   section.form
-    b-field( label="Department Full Name")
-      b-input( :value="$props.dept_name_long" )
+    b-field( label="Location Distuct")
+      b-input( :value="$props.loc_dist" )
       
     b-field( label="Department Short Name")
       b-input( :value="$props.dept_name_short" )
-  
+
+    b-field( label="")
+      b-input(:value="")
+
+
 </template>
 <script>
 export default {
   props:{
-    loc_dist : String,
-    loc_subdist : String
+    loc_dist : {
+      type: String,
+      default: "Hong Kong"
+    },
+    loc_subdist :{
+      type:String,
+      default : "",
+    } 
   }
 }
 </script>
